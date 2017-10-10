8c3cdfb6f8d16e40df859c873ac9950a0e5b2200
ikantemirov <ilia.kantemirov@gmail.com>
2017-10-10 23:43:36 +0300

    commit with google search

--- src/test/java/io/ikantemirov/webtest/WebTest.java
+++ src/test/java/io/ikantemirov/webtest/WebTest.java
@@ -2,6 +2,8 @@ package io.ikantemirov.webtest;
 
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,6 +13,7 @@ public class WebTest {
     @Test
     public void searchByRequest() {
         WebDriver driver = new ChromeDriver();
-        driver.get("http://www.yandex.ru");
+        driver.get("http://www.google.com");
+        driver.findElement(By.id("lst-ib")).sendKeys("Artem Eroshenko" + Keys.ENTER);
     }
 }
