12330454afee0a4422107e10c731970eb53a2402
ikantemirov <ilia.kantemirov@gmail.com>
2017-10-11 00:10:03 +0300

    commit with 3 search test methods

--- src/test/java/io/ikantemirov/webtest/WebTest.java
+++ src/test/java/io/ikantemirov/webtest/WebTest.java
@@ -1,6 +1,7 @@
 package io.ikantemirov.webtest;
 
 
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -11,9 +12,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebTest {
 
     @Test
-    public void searchByRequest() {
+    public void searchByRequestOnYandex() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.yandex.ru");
+        driver.findElement(By.id("text")).sendKeys("Илья Молодец!");
+        driver.findElement(By.xpath("//div[@class='search2__button']//button")).click();
+    }
+
+    @Test
+    public void searchByRequestOnGoogle() {
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
         driver.findElement(By.id("lst-ib")).sendKeys("Artem Eroshenko" + Keys.ENTER);
+    }
+
+   @Test
+    public void searchByRequestOnWikipedia() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://ru.wikipedia.org");
+        driver.findElement(By.id("searchInput")).sendKeys("Петр 1");
+        driver.findElement(By.id("searchButton")).click();
     }
 }
