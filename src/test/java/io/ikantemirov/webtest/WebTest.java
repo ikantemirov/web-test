714228fcb05984f8ccb1ec9ebaef5565b12a4a83
ikantemirov <ilia.kantemirov@gmail.com>
2017-10-11 00:44:32 +0300

    add before & after methods

--- src/test/java/io/ikantemirov/webtest/WebTest.java
+++ src/test/java/io/ikantemirov/webtest/WebTest.java
@@ -2,6 +2,8 @@ package io.ikantemirov.webtest;
 
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -10,10 +12,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class WebTest {
+    WebDriver driver;
+
+    @Before
+    public void openDriver() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void closeDriver() {
+        driver.quit();
+    }
+
 
     @Test
     public void searchByRequestOnYandex() {
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
         driver.get("http://www.yandex.ru");
         driver.findElement(By.id("text")).sendKeys("Илья Молодец!");
         driver.findElement(By.xpath("//div[@class='search2__button']//button")).click();
@@ -21,14 +35,14 @@ public class WebTest {
 
     @Test
     public void searchByRequestOnGoogle() {
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
         driver.findElement(By.id("lst-ib")).sendKeys("Artem Eroshenko" + Keys.ENTER);
     }
 
    @Test
     public void searchByRequestOnWikipedia() {
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
         driver.get("http://ru.wikipedia.org");
         driver.findElement(By.id("searchInput")).sendKeys("Петр 1");
         driver.findElement(By.id("searchButton")).click();
